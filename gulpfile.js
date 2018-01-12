caa62e6d83f545ca2c7c8731f9c7d9d813b8f562
Chris Harvey <chrisharvey2pi@gmail.com>
2018-01-12 12:22:15 -0500

    renamed:    config-jsdoc.json -> jsdoc.config.json

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ const jsdoc = require('gulp-jsdoc3')
 
 gulp.task('docs:api', function () {
   return gulp.src(['README.md', './index.js', 'src/Color.class.js'], {read: false})
-    .pipe(jsdoc(require('./config-jsdoc.json')))
+    .pipe(jsdoc(require('./jsdoc.config.json')))
 })
 
 gulp.task('build', ['docs:api'])
