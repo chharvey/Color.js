768234d61adbb97934d9081bc230563c91215554
Chris Harvey <chrisharvey2pi@gmail.com>
2018-10-17 14:20:25 -0400

    add test files

--- gulpfile.js
+++ gulpfile.js
@@ -21,6 +21,14 @@ gulp.task('test-out', async function () {
 
 gulp.task('test-run', async function () {
 	await Promise.all([
+		require('./test/out/Color--fromString.test.js').default,
+		require('./test/out/Color--random.test.js').default,
+		require('./test/out/Color--randomName.test.js').default,
+		require('./test/out/Color-constructor.test.js').default,
+		require('./test/out/Color-toString.test.js').default,
+		require('./test/out/Color-rotate.test.js').default,
+		require('./test/out/Color-invert.test.js').default,
+		require('./test/out/Color-name.test.js').default,
 	])
 	console.info('All tests ran successfully!')
 })
