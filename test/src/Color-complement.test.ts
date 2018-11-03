de295d932b1c59bb67fd09db2e848b9692b374b6
Chris Harvey <chrisharvey2pi@gmail.com>
2018-11-02 21:15:31 -0400

    fixup tests

--- test/src/Color-complement.test.ts
+++ test/src/Color-complement.test.ts
@@ -3,5 +3,5 @@ import test from './test'
 
 
 export default Promise.all([
-	test(new Color(128,0,0).complement().toString(), '#008080'),
+	test(new Color(0.5,0,0).complement().toString(), '#008080'),
 ])
